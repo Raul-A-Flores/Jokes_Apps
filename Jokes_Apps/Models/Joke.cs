8de1b0d7fb9e5cbd25327bb549ff648860401d1b
Raul-A-Flores <raul.alexander.floress@gmail.com>
2024-03-03 11:06:33 -0800

    first commit

---
+++ Jokes_Apps/Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿namespace Jokes_Apps.Models
+{
+    public class Joke
+    {
+
+        public int Id { get; set; }
+        public string JokeName { get; set; }
+        public string JokeAnswer { get; set; }
+
+
+        public Joke()
+        {
+            
+        }
+    }
+}
